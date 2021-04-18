571b9b02b915785964f8e975d772d80dae4a66d6
Nek <68248147+NekWasTaken@users.noreply.github.com>
2021-04-17 18:48:36 -0700

    Update event.js

--- handler/event.js
+++ handler/event.js
@@ -1,13 +1,7 @@
-const { readdirSync } = require("fs")
+/* 
 
-module.exports = (bot) => {
-    const load = dirs => {    
-        const events = readdirSync(`./events/${dirs}/`).filter(d => d.endsWith('.js'));
-        for (let file of events) {
-            const evt = require(`../events/${dirs}/${file}`);
-            let eName = file.split('.')[0];
-            bot.on(eName, evt.bind(null, bot));
-          };
-        };
-        ["client", "guild"].forEach(x => load(x));
-};
+
+You seriously thought that this was an actual file.
+
+
+*/ 
